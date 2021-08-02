efe884c3e6cfa6564bcef1893cebfe2cc11e4bb4
Mochamad Idris <idrez.mochamad@gmail.com>
2021-08-02 17:52:07 +0700

    fix cronjob.sh

--- cronjob.sh
+++ cronjob.sh
@@ -2,4 +2,4 @@
 cur_dir="$(pwd)"
 php "${cur_dir}/public/index.php" "pub/cron_auto_close" >> "${cur_dir}/log/log_cron_auto_close.log"
 php "${cur_dir}/public/index.php" "pub/cron_auto_start_ujian_for_unstarted_participants" >> "${cur_dir}/log/log_cron_auto_start_ujian_for_unstarted_participants.log"
-php "${cur_dir}/public/index.php" "auth/cron_auto_registrasi" >> "${cur_dir}/log/tryout/log_cron_auto_registrasi.log"
+php "${cur_dir}/public/index.php" "auth/cron_auto_registrasi" >> "${cur_dir}/log/log_cron_auto_registrasi.log"
